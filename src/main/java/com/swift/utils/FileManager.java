5e9238a23ad857ff74af666af644424e32048c63
Shuu-Maiko <priyanshunegi246@gmail.com>
2026-04-16 16:20:19 +0530

    Implement SHA-256 hash calculation in FileManager Added calculateSHA256 method using java.security.MessageDigest This will be used for file integrity verification

--- src/main/java/com/swift/utils/FileManager.java
+++ src/main/java/com/swift/utils/FileManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class FileManager {
   public static boolean isFileValid(String path) {
@@ -27,5 +29,22 @@ public class FileManager {
 
   public static FileOutputStream getOutputStream(String filename) throws IOException {
     return new FileOutputStream(filename);
+  }
+
+  public static String calculateSHA256(String path) throws IOException, NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    try (FileInputStream fis = new FileInputStream(path)) {
+      byte[] byteArray = new byte[1024];
+      int bytesCount = 0;
+      while ((bytesCount = fis.read(byteArray)) != -1) {
+        digest.update(byteArray, 0, bytesCount);
+      }
+    }
+    byte[] bytes = digest.digest();
+    StringBuilder sb = new StringBuilder();
+    for (byte b : bytes) {
+      sb.append(String.format("%02x", b));
+    }
+    return sb.toString();
   }
 }
